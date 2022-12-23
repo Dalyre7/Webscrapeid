a7ff082921a7182d4877e5ae2f6a465e5a07f2ef
Dalyre7 <110495237+Dalyre7@users.noreply.github.com>
2022-12-23 14:35:37 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+
+
+
+from termcolor import colored
+from pyfiglet import Figlet
+
+f = Figlet(font='standard')
+print(colored(f.renderText("Webscraperid"), 'red'))
+
+
+#this is a work in progress
+
+
+
+
+
+
+
+
+
